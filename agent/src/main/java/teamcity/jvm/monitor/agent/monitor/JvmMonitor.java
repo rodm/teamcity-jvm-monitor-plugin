d78a3ee32afbcf2aea415328155127b07ac7ef62
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2015-04-14 14:24:21 +0100

    Changes to use the vmStatusChanged event to start and stop monitoring of JVMs.

--- agent/src/main/java/teamcity/jvm/monitor/agent/monitor/JvmMonitor.java
+++ agent/src/main/java/teamcity/jvm/monitor/agent/monitor/JvmMonitor.java
@@ -8,25 +8,23 @@ import sun.jvmstat.monitor.MonitoredVmUtil;
 import sun.jvmstat.monitor.VmIdentifier;
 import sun.jvmstat.monitor.event.HostEvent;
 import sun.jvmstat.monitor.event.HostListener;
-import sun.jvmstat.monitor.event.MonitorStatusChangeEvent;
-import sun.jvmstat.monitor.event.VmEvent;
-import sun.jvmstat.monitor.event.VmListener;
 import sun.jvmstat.monitor.event.VmStatusChangeEvent;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
-public class JvmMonitor implements Runnable, VmListener, HostListener {
+public class JvmMonitor implements HostListener {
+
+    private static final int DEFAULT_POOL_SIZE = 4;
 
     private final File outputDir;
 
@@ -38,10 +36,13 @@ public class JvmMonitor implements Runnable, VmListener, HostListener {
 
     public JvmMonitor(File outputDir) {
         this.outputDir = outputDir;
-        this.executor = Executors.newScheduledThreadPool(4);
+    }
+
+    public void start() {
+        executor = Executors.newScheduledThreadPool(DEFAULT_POOL_SIZE);
         try {
-            this.monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
-            this.monitoredHost.addHostListener(this);
+            monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
+            monitoredHost.addHostListener(this);
         } catch (MonitorException e) {
             e.printStackTrace(System.err);
         } catch (URISyntaxException e) {
@@ -49,85 +50,66 @@ public class JvmMonitor implements Runnable, VmListener, HostListener {
         }
     }
 
-    public void start() {
-        executor.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
-    }
-
     public void stop() {
-        for (JvmDataCollector collector : monitoredVms.values()) {
-            collector.stop();
+        for (Integer vmId : new HashSet<Integer>(monitoredVms.keySet())) {
+            stopCollector(vmId);
+        }
+        try {
+            monitoredHost.removeHostListener(this);
+        } catch (MonitorException e) {
+            e.printStackTrace(System.err);
         }
         executor.shutdown();
     }
 
     @Override
-    public void run() {
-        try {
-            Set<Integer> activeVms = monitoredHost.activeVms();
-            for (Integer id : activeVms) {
-                MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(id.toString()));
-
-                if (!monitoredVms.containsKey(id)) {
-                    System.out.println("Adding JVM(pid=" + id + ") to monitored list");
-                    String mainClass = MonitoredVmUtil.mainClass(mvm, true);
-                    mainClass = mainClass.replace('/', '.');
-                    mainClass = mainClass.trim();
-                    String name = id + "-" + mainClass + ".txt";
-                    File outputFile = new File(outputDir, name);
-                    Writer writer = new FileWriter(outputFile);
-                    JvmDataCollector collector = new JvmDataCollector(executor, mvm, writer);
-                    monitoredVms.put(id, collector);
-                    mvm.addVmListener(this);
-                }
-            }
-        } catch (MonitorException e) {
-            e.printStackTrace(System.err);
-        } catch (URISyntaxException e) {
-            e.printStackTrace(System.err);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.err);
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
+    public void vmStatusChanged(VmStatusChangeEvent event) {
+        Set<Integer> startedVMs = event.getStarted();
+        for (Integer id : startedVMs) {
+            startCollector(id);
         }
-    }
-
-    @Override
-    public void monitorStatusChanged(MonitorStatusChangeEvent event) {
-    }
-
-    @Override
-    public void monitorsUpdated(VmEvent event) {
-    }
-
-    @Override
-    public void disconnected(VmEvent event) {
-        int id = event.getMonitoredVm().getVmIdentifier().getLocalVmId();
-        System.out.println("JVM pid=" + id + " disconnected");
-
-        if (monitoredVms.containsKey(id)) {
-            System.out.println("Removing JVM(pid=" + id + ") from monitored list");
-            JvmDataCollector collector = monitoredVms.remove(id);
-            collector.stop();
-            try {
-                event.getMonitoredVm().removeVmListener(this);
-            } catch (MonitorException e) {
-                e.printStackTrace(System.err);
-            }
-        }
-    }
-
-    @Override
-    public void vmStatusChanged(VmStatusChangeEvent ev) {
-        Set<Integer> terminated = ev.getTerminated();
-        for (Integer vm : terminated) {
-            System.out.println("vm terminated: " + vm);
+        Set<Integer> terminatedVMs = event.getTerminated();
+        for (Integer id : terminatedVMs) {
+            stopCollector(id);
         }
     }
 
     @Override
     public void disconnected(HostEvent event) {
-        if (monitoredHost == event.getMonitoredHost()) {
-            System.out.println("host: " + event.getMonitoredHost());
+    }
+
+    private void startCollector(Integer id) {
+        try {
+            MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(id.toString()));
+
+            if (!monitoredVms.containsKey(id)) {
+                System.out.println("Starting collector for VM: " + id);
+                String mainClass = MonitoredVmUtil.mainClass(mvm, true);
+                mainClass = mainClass.replace('/', '.');
+                mainClass = mainClass.trim();
+                String name = id + "-" + mainClass + ".txt";
+                File outputFile = new File(outputDir, name);
+                Writer writer = new FileWriter(outputFile);
+                JvmDataCollector collector = new JvmDataCollector(executor, mvm, writer);
+                monitoredVms.put(id, collector);
+            }
+        }
+        catch (MonitorException e) {
+            e.printStackTrace(System.err);
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace(System.err);
+        }
+        catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private void stopCollector(Integer id) {
+        System.out.println("Stopping collector for VM: " + id);
+        JvmDataCollector collector = monitoredVms.remove(id);
+        if (collector != null) {
+            collector.stop();
         }
     }
 }
