51357c3b69bb07d77fa23b551af9b930cb866845
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2015-12-08 13:30:42 +0000

    Use the log4j jar from the TeamCity distribution
    Also improved the logging of the command line used to launch the
    JVM monitor process.

--- agent/src/main/java/teamcity/jvm/monitor/agent/JvmMonitorLauncher.java
+++ agent/src/main/java/teamcity/jvm/monitor/agent/JvmMonitorLauncher.java
@@ -36,22 +36,22 @@ public class JvmMonitorLauncher {
         String javaHome = System.getProperty("java.home") + "/..";
         File javaCommand = new File(javaHome, "bin/java");
         File agentJar = new File(JvmMonitorLauncher.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        File log4jJar = new File(Logger.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         File toolsJar = new File(javaHome, "lib/tools.jar");
-        File log4jJar = new File(agentJar.getParentFile(), "log4j-1.2.17.jar");
 
         String classPath = toolsJar.getCanonicalPath() + File.pathSeparator +
                 agentJar.getCanonicalPath() + File.pathSeparator +
                 log4jJar.getCanonicalPath();
-        List<String> args = new ArrayList<String>();
-        args.add(javaCommand.getAbsolutePath());
-        args.add("-cp");
-        args.add(classPath);
-        args.add(JvmMonitorMain.class.getName());
-        args.add(logDir.getCanonicalPath());
-        args.add(outputDir.getCanonicalPath());
+        List<String> commandLine = new ArrayList<String>();
+        commandLine.add(javaCommand.getAbsolutePath());
+        commandLine.add("-cp");
+        commandLine.add(classPath);
+        commandLine.add(JvmMonitorMain.class.getName());
+        commandLine.add(logDir.getCanonicalPath());
+        commandLine.add(outputDir.getCanonicalPath());
         ProcessBuilder builder = new ProcessBuilder();
-        builder.command(args);
-        LOGGER.info("command line: " + builder.toString());
+        builder.command(commandLine);
+        LOGGER.info("JVM Monitor command line: " + commandLine);
 
         process = builder.start();
         OutputStream in = process.getOutputStream();
