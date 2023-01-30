3b9414a0e4919e34f7c025f0ef16fe001d9a6f9c
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-01-30 13:43:38 +0000

    Build TeamCity components with Java 8

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -6,8 +6,9 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 base {
