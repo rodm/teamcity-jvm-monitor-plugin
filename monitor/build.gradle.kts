378747bbc33c61193a94a320d13a7ec6b201ddaa
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-02-01 12:51:09 +0000

    Use a Java 7 toolchain for compiling and a Java 8 toolchain for testing

--- monitor/build.gradle.kts
+++ monitor/build.gradle.kts
@@ -9,23 +9,25 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 base {
     archivesName.set("jvm-monitor-tool")
 }
 
-val javaHome = System.getProperty("java.home")
+val java7Compiler = javaToolchains.compilerFor { languageVersion.set(JavaLanguageVersion.of(7)) }
+val java7Home = java7Compiler.get().metadata.installationPath.toString()
 
 dependencies {
-    compileOnly (files("${javaHome}/../lib/tools.jar"))
+    compileOnly (files("${java7Home}/lib/tools.jar"))
     implementation (group = "org.apache.logging.log4j", name = "log4j-1.2-api", version = "2.12.4")
     implementation (group = "org.apache.logging.log4j", name = "log4j-api", version = "2.12.4")
     implementation (group = "org.apache.logging.log4j", name = "log4j-core", version = "2.12.4")
 
-    testImplementation (files("${javaHome}/../lib/tools.jar"))
+    testImplementation (files("${java7Home}/lib/tools.jar"))
     testImplementation (platform("org.junit:junit-bom:5.5.2"))
     testImplementation (group = "org.junit.jupiter", name = "junit-jupiter-api")
     testImplementation (group = "org.junit.jupiter", name = "junit-jupiter-params")
@@ -36,6 +38,10 @@ dependencies {
 }
 
 tasks {
+    compileJava {
+        javaCompiler.set(java7Compiler)
+    }
+
     test {
         useJUnitPlatform()
         finalizedBy (named("jacocoTestReport"))
