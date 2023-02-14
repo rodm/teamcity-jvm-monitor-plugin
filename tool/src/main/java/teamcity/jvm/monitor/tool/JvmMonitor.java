b01ac7526ca53a3fb2e716de6174c18857bbce2c
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-02-14 12:01:26 +0000

    Use a default name for JVM log files when main class not returned

--- tool/src/main/java/teamcity/jvm/monitor/tool/JvmMonitor.java
+++ tool/src/main/java/teamcity/jvm/monitor/tool/JvmMonitor.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -106,9 +106,7 @@ public class JvmMonitor implements HostListener {
             MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(id.toString()));
 
             if (!monitoredVms.containsKey(id)) {
-                String mainClass = MonitoredVmUtil.mainClass(mvm, true);
-                mainClass = mainClass.replace('/', '.');
-                mainClass = mainClass.trim();
+                String mainClass = getMainClass(mvm);
                 LOGGER.info("Starting collector for VM: " + id + ": main class: " + mainClass);
                 String name = id + "-" + mainClass + ".txt";
                 File outputFile = new File(outputDir, name);
@@ -134,5 +132,11 @@ public class JvmMonitor implements HostListener {
         if (collector != null) {
             collector.stop();
         }
+    }
+
+    private static String getMainClass(MonitoredVm mvm) throws MonitorException {
+        String mainClass = MonitoredVmUtil.mainClass(mvm, true);
+        mainClass = mainClass.replace('/', '.').trim();
+        return mainClass.isEmpty() ? "unknown" : mainClass;
     }
 }
