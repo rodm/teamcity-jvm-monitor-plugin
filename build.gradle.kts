5020b9f214ec88a664e466d6ffc11fe743fb3611
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-02-28 09:34:11 +0000

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.rodm"
-version = "1.0-beta-4"
+version = "1.0-SNAPSHOT"
 
 extra["teamcityVersion"] = project.findProperty("teamcity.api.version") as String? ?: "2018.1"
 
