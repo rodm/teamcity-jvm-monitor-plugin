0663bd3dc08a4b9951a755e2d8f8279d52d16f30
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-04-03 13:22:35 +0100

    Disable SonarQube warning about using Sun classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,8 @@ sonarqube {
     properties {
         property("sonar.projectKey", "${project.group}:teamcity-jvm-monitor-plugin")
         property("sonar.projectName", "teamcity-jvm-monitor-plugin")
+        property("sonar.issue.ignore.multicriteria", "e1")
+        property("sonar.issue.ignore.multicriteria.e1.ruleKey", "java:S1191")
+        property("sonar.issue.ignore.multicriteria.e1.resourceKey", "**/*.java")
     }
 }
