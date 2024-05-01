e58948045a9860235d601892408b1e3c9f76beb0
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2024-05-01 16:24:46 +0100

    Update API version

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot.AgentCheckoutPolicy.NO_MIRRORS
 import jetbrains.buildServer.configs.kotlin.version
 
-version = "2023.05"
+version = "2024.03"
 
 project {
     description = "A TeamCity plugin that collects JVM metrics during a build"
