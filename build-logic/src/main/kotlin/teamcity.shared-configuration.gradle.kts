d5c3984e115a7e32265cc6c18acd75f720a3d8ff
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2024-12-21 16:53:00 +0000

    Update dependencies

--- build-logic/src/main/kotlin/teamcity.shared-configuration.gradle.kts
+++ build-logic/src/main/kotlin/teamcity.shared-configuration.gradle.kts
@@ -11,12 +11,13 @@ java {
 }
 
 dependencies {
-    testImplementation (platform("org.junit:junit-bom:5.10.2"))
+    testImplementation (platform("org.junit:junit-bom:5.11.4"))
     testImplementation (group = "org.junit.jupiter", name = "junit-jupiter-api")
     testImplementation (group = "org.junit.jupiter", name = "junit-jupiter-params")
-    testImplementation (group = "org.hamcrest", name = "hamcrest", version = "2.2")
+    testImplementation (group = "org.hamcrest", name = "hamcrest", version = "3.0")
     testImplementation (group = "org.mockito", name = "mockito-core", version = "4.11.0")
 
+    testRuntimeOnly (group = "org.junit.platform", name = "junit-platform-launcher")
     testRuntimeOnly (group = "org.junit.jupiter", name = "junit-jupiter-engine")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,12 +9,12 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             library("json-api", "jakarta.json:jakarta.json-api:2.1.3")
-            library("json-impl", "org.eclipse.parsson:parsson:1.1.6")
+            library("json-impl", "org.eclipse.parsson:parsson:1.1.7")
 
             version("log4j", "1.2.17")
             library("log4j", "log4j", "log4j").versionRef("log4j")
 
-            version("log4j2", "2.23.1")
+            version("log4j2", "2.24.3")
             library("log4j2-api", "org.apache.logging.log4j", "log4j-api").versionRef("log4j2")
             library("log4j2-core", "org.apache.logging.log4j", "log4j-core").versionRef("log4j2")
             bundle("log4j2", listOf("log4j2-api", "log4j2-core"))
