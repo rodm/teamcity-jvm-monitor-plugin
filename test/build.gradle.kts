c238c25f84f9ec637b878167694e4fdc519b7c14
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2024-12-22 17:05:40 +0000

    Tidy environment configuration
    Removed unnecessary extra properties and moved debug settings
    to share with multiple environments.

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     id ("teamcity.environments")
 }
 
-extra["downloadsDir"] = project.findProperty("downloads.dir") as String? ?: "$rootDir/downloads"
-extra["serversDir"] = project.findProperty("servers.dir") as String? ?: "$rootDir/servers"
-extra["java11Home"] = project.findProperty("java11.home") ?: "/opt/jdk-11.0.2"
-
 val plugins by configurations.creating
 
 dependencies {
@@ -15,23 +11,25 @@ dependencies {
 
 teamcity {
     environments {
-        downloadsDir = extra["downloadsDir"] as String
-        baseHomeDir = extra["serversDir"] as String
         baseDataDir = "${rootDir}/data"
+
+        val java11Home = project.findProperty("java11.home") as String? ?: "/opt/jdk-11.0.2"
+        val serverDebugOptions = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+        val agentDebugOptions = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
 
         register("teamcity2018.1") {
             version = "2018.1.5"
             plugins = configurations["plugins"]
-            serverOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005")
-            agentOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006")
+            serverOptions (serverDebugOptions)
+            agentOptions (agentDebugOptions)
         }
 
         register("teamcity2024.03") {
-            version = "2024.03"
-            javaHome = extra["java11Home"] as String
+            version = "2024.03.3"
+            javaHome = java11Home
             plugins = configurations["plugins"]
-            serverOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005")
-            agentOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006")
+            serverOptions (serverDebugOptions)
+            agentOptions (agentDebugOptions)
         }
     }
 }
