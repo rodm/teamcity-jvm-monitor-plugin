08d031176902142a3c6267f25db52a999ef0dbdc
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2025-07-31 11:03:20 +0100

    Update test environment to use TeamCity 2025.07
    Changed environment to use Java 21 to run the server and agent.

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 
 teamcity {
     environments {
-        val java11Home = project.findProperty("java11.home") as String? ?: "/opt/jdk-11.0.2"
+        val java21Home = project.findProperty("java21.home") as String? ?: "/opt/jdk-21.0.2"
         val serverDebugOptions = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
         val agentDebugOptions = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
 
@@ -22,9 +22,9 @@ teamcity {
             agentOptions (agentDebugOptions)
         }
 
-        register("teamcity2025.03") {
-            version = "2025.03"
-            javaHome = java11Home
+        register("teamcity2025.07") {
+            version = "2025.07"
+            javaHome = java21Home
             plugins = configurations["plugins"]
             serverOptions (serverDebugOptions)
             agentOptions (agentDebugOptions)
