f9ced039126a0c2ce5a6dc25b7cbd10744906453
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2026-05-20 16:05:28 +0100

    Add Java 26 to test list and remove other non LTS versions

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -27,7 +27,7 @@ tasks {
         from(tool)
     }
 
-    val javaVersions = listOf(8, 11, 17, 21, 22, 23, 24, 25)
+    val javaVersions = listOf(8, 11, 17, 21, 25, 26)
     register<Test>("functionalTest") {
         group = "verification"
         description = "Runs the functional tests."
