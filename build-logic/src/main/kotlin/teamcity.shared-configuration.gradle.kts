23ad658f39c083e4aae0326daab2b63590ef4589
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2026-09-09 14:28:21 +0100

    Update libraries

--- build-logic/src/main/kotlin/teamcity.shared-configuration.gradle.kts
+++ build-logic/src/main/kotlin/teamcity.shared-configuration.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    testImplementation (platform("org.junit:junit-bom:6.1.0"))
+    testImplementation (platform("org.junit:junit-bom:6.1.3"))
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-params")
     testImplementation ("org.hamcrest:hamcrest:3.0")
